e01c1e435fd8ae3d99560db00729ab77f4bda328
Miha Krajnc <miha.krajnc@dewesoft.com>
2023-01-03 09:21:02 +0100

    Update sql init

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.credentials(username, "password") VALUES ('mihoci10', 'asdf1234');
-INSERT INTO public.details(name, surname, email, gender) VALUES ('Miha', 'Krajnc', 'mk7793@student.uni-lj.si', 'Male');
+INSERT INTO public.credentials(username, "password") VALUES ('test', 'asdf1234');
+INSERT INTO public.details(name, surname, email, gender) VALUES ('Test', 'TestSurname', 'test@hotmail.com', 'Male');
 INSERT INTO public.users("detailsId", "credId") VALUES (1,1);
